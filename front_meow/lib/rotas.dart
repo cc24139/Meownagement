d5232ab7ac1941c8e7fe9cbc36ababf2bde17986
Júlio Pacheco Stein <juliopstein@gmail.com>
2025-09-15 08:33:49 -0300

    arquivo de rotas criado

---
+++ front_meow/lib/rotas.dart
@@ -0,0 +1,12 @@
+class appRotas {
+  static const String home = '/';
+  static const String login = '/login';
+  static const String cadastro = '/cadastro';
+  static const String alterarSenha = '/alterarSenha';
+  static const String perfil = '/perfil';
+  static const String configPerfil = '/configPerfil';
+  static const String galeria = '/galeria';
+  static const String gato = '/gato';
+  static const String pessoas = '/pessoas';
+  static const String gacha = '/gacha';
+}
