5493715848c27ca513b375235d69ed5187026c36
Júlio Pacheco Stein <juliopstein@gmail.com>
2025-09-19 12:23:20 -0300

    Update serv.dart

--- front_meow/lib/services/serv.dart
+++ front_meow/lib/services/serv.dart
@@ -1,20 +1,38 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:front_meow/model/usuario.dart';
 
 class Http {
   static String url = "http://"; // colocar a url da api
 
   // exemplo -> dps poem as rotas reias aqui
-  static getUsuario() async {
-    final res = await http.get(Uri.parse("#{url}/usuario"));
+  Future<List<Usuario>> fetchUsuarios() async {
+    final response = await http.get(Uri.parse('${url}/usuarios'));
 
-    if (res.statusCode == 200) {
-      
-      var data = jsonDecode(res.body.toString());
-      return data;
-
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => Usuario.fromJson(data)).toList();
     } else {
-      throw Exception("Erro ao carregar usuarios");
+      throw Exception('Erro ao carregar usuários');
     }
   }
+
+  /*
+
+  para chamar nas telas:
+
+  const Http = Http(); // classe http para usar as rotas
+
+  var usuarios = await Http.fetchUsuarios();
+
+  // ou
+
+  await Http.fetchUsuarios().then((value) {
+    usuarios = value;
+  });
+
+  print(usuarios[0].nome);
+
+
+  */
 }
