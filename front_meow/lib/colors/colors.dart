d86452c44af55b3bd98e641f0d1dfcad72ff0711
EduardoTribst <eduardo.tribst@gmail.com>
2025-09-23 11:15:24 -0300

    paletas adicionadas

--- front_meow/lib/colors/colors.dart
+++ front_meow/lib/colors/colors.dart
@@ -9,6 +9,21 @@ class CatColors {
 
   CatColors({required this.paleta}) {
     switch (paleta.toLowerCase()) {
+      case 'Da':
+        primaria = Color(0xFFF8EDEB);
+        secundaria = Color(0xFFFFB5A7);
+        tercearia = Color(0xFFFCD5CE);
+        complementar = Colors.white;
+      case 'Ni':
+        primaria = Color(0xFFECF0F1);
+        secundaria = Color(0xFF2C3E50);
+        tercearia = Color(0xFF34495E);
+        complementar = Colors.white;
+      case 'El':
+        primaria = Color(0xFF166534);
+        secundaria = Color(0xFFF5DEB3);
+        tercearia = Color(0xFFEA580C);
+        complementar = Colors.white;
       case '':
         primaria     = Color(0xFF374151);
         secundaria   = Color(0xFFF97316);
