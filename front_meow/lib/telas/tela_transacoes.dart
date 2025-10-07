bec1f43a02e974aa07a6dc69786743bd23229aa5
Eduardo Tribst <eduardo.tribst@gmail.com>
2025-10-06 22:15:15 -0300

    começo tela transações

--- front_meow/lib/telas/tela_transacoes.dart
+++ front_meow/lib/telas/tela_transacoes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class TelaTransacoes extends StatefulWidget {
   const TelaTransacoes({super.key});
@@ -10,6 +11,22 @@ class TelaTransacoes extends StatefulWidget {
 class _TelaTransacoesState extends State<TelaTransacoes> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text("Planeje transações"),
+            Row(
+              children: [
+                SvgPicture.asset("../../assets/icons/vetor_olho_aberto.svg"),
+                Text("Saldo")
+              ],
+            ),
+          ],
+        ),
+      ),
+      
+    );
   }
 }
