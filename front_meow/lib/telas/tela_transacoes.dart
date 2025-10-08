38bc20f4300f870e1259d932bbc90465f4f9c17f
EduardoTribst <eduardo.tribst@gmail.com>
2025-10-08 14:29:46 -0300

    um pouco da tela de transacoes

--- front_meow/lib/telas/tela_transacoes.dart
+++ front_meow/lib/telas/tela_transacoes.dart
@@ -8,7 +8,28 @@ class TelaTransacoes extends StatefulWidget {
   State<TelaTransacoes> createState() => _TelaTransacoesState();
 }
 
+enum OpcoesTransacao { despesa, receita }
+
+
 class _TelaTransacoesState extends State<TelaTransacoes> {
+  OpcoesTransacao? _opcoesTransacao = OpcoesTransacao.despesa;
+  TextEditingController _dateController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),  
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        _dateController.text = "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +43,40 @@ class _TelaTransacoesState extends State<TelaTransacoes> {
                 SvgPicture.asset("../../assets/icons/vetor_olho_aberto.svg"),
                 Text("Saldo")
               ],
+            ),
+            Text("Tipo de transação / Data da início"),
+            Column(
+              children: <Widget> [
+                RadioListTile<OpcoesTransacao>(
+                  title: const Text('Despesa'), // The label for the radio button
+                  value: OpcoesTransacao.despesa,
+                  groupValue: _opcoesTransacao,
+                  onChanged: (OpcoesTransacao? value) {
+                    setState(() {
+                      _opcoesTransacao = value;
+                    });
+                  },
+                ),
+                RadioListTile<OpcoesTransacao>(
+                  title: const Text("Receita"),
+                  value: OpcoesTransacao.receita,
+                  groupValue: _opcoesTransacao,
+                  onChanged: (OpcoesTransacao? value) {
+                    setState(() {
+                      _opcoesTransacao = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            TextFormField(
+              controller: _dateController,
+              readOnly: true,
+              decoration: InputDecoration(
+                labelText: 'Select Date',
+                suffixIcon: Icon(Icons.calendar_today),
+              ),
+              onTap: () => _selectDate(context),
             ),
           ],
         ),
