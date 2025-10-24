a57289221d272f92aa4989686945ad41b41a0d6f
EduardoTribst <eduardo.tribst@gmail.com>
2025-10-24 12:09:52 -0300

    alterar senha também com layout

--- front_meow/lib/telas/tela_alterar_senha.dart
+++ front_meow/lib/telas/tela_alterar_senha.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:front_meow/Widgets/BolasLogin.dart';
+import 'package:front_meow/Widgets/ElevatedButtonWidget.dart';
+import 'package:front_meow/Widgets/Tools/ButtonSize.dart';
+import 'package:front_meow/colors/colors.dart';
 import 'package:front_meow/rotas.dart';
 import 'package:front_meow/services/UsuarioServices.dart';
 
@@ -24,71 +28,165 @@ class _TelaAlterarSenhaState extends State<TelaAlterarSenha> {
 
   @override
   Widget build(BuildContext context) {
+    CatColors cores = CatColors(paleta: 2);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Alterar Senha",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            fontFamily: "Arial",
-          ),
-        ),
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 250,
-                  child: TextField(
-                    controller: txtEmail,
-                    decoration: const InputDecoration(
-                      labelText: "Digite seu Email",
-                      hintText: "email@exemplo.com",
-                    ),
-                  ),
-                ),
-
-                SizedBox(
-                  width: 250,
-                  child: TextField(
-                    controller: txtNovaSenha,
-                    decoration: const InputDecoration(
-                      labelText: "Digite sua Nova Senha",
-                      hintText: "Nova Senha",
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                SizedBox(
-                  width: 250,
-                  child: TextField(
-                    controller: txtConfirmarSenha,
-                    decoration: const InputDecoration(
-                      labelText: "Confirme sua Nova Senha",
-                      hintText: "Confirmação da Nova Senha",
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    UsuarioServices().EsqueceuSenhaUsuario(txtEmail.text, txtNovaSenha.text);
-                    Navigator.pushReplacementNamed(context, AppRotas.confirmacao, arguments: {'email': txtEmail.text, 'isCreateCount': false});
-                  },
-                  child: const Text("Alterar Senha"),
-                ),
-              ],
+      backgroundColor: cores.corPrimaria,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: ClipPath(
+              clipper: BolasLogin(),
+              child: Container(
+                height: 175,
+                color: cores.corSecundaria.withOpacity(0.9),
+              ),
             ),
           ),
-        ),
-      )
+
+          Center(
+            child: SingleChildScrollView(
+              child: Center(
+                child: SizedBox(
+                  width: 300,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Alterar Senha",
+                            style: TextStyle(
+                              color: cores.secundaria,
+                              fontSize: 45,
+                              fontFamily: "Londrina",
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 80),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Email",
+                            style: TextStyle(
+                              color: cores.secundaria,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextField(
+                        controller: txtEmail,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: cores.tercearia,
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: cores.tercearia,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Senha",
+                            style: TextStyle(
+                              color: cores.secundaria,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextField(
+                        controller: txtNovaSenha,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: cores.tercearia,
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: cores.tercearia,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Confirmar senha",
+                            style: TextStyle(
+                              color: cores.secundaria,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      TextField(
+                        controller: txtConfirmarSenha,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: cores.tercearia,
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: cores.tercearia,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 70,
+                      ),
+                      ElevatedButtonWidget(
+                        text: "Cadastrar",
+                        onPressed: () {
+                          UsuarioServices().EsqueceuSenhaUsuario(txtEmail.text, txtNovaSenha.text);
+                          Navigator.pushReplacementNamed(context, AppRotas.confirmacao, arguments: {'email': txtEmail.text, 'isCreateCount': false});
+                        },
+                        highSize: ButtonSize.grande,
+                        widthSize: ButtonSize.grande,
+                        catColors: cores,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
